5e2468eb1046c834de2e79af40eb6c14548e02bd
trippl <tlaursen8@yahoo.com>
2024-05-19 20:22:30 -0400

    Sequences Lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+    
+    fibonocci_sequence = [0, 1]
+    while len(fibonocci_sequence) < length:
+        fibonocci_sequence.append(fibonocci_sequence[-1] + fibonocci_sequence[-2])
+        
+    print(fibonocci_sequence)
